a9e87e0daad5aa532ea9e4e5a111349dd46bfd2b
xtm <2083933996@qq.com>
2025-08-04 20:55:56 +0800

    feat-8/4

--- flags/enter.go
+++ flags/enter.go
@@ -10,6 +10,7 @@ type Options struct {
 	File    string
 	DB      bool
 	Version bool
+	User    string
 }
 
 var FileOption = new(Options)
@@ -18,6 +19,7 @@ func Parse() {
 	flag.StringVar(&FileOption.File, "f", "settings.yaml", "配置文件")
 	flag.BoolVar(&FileOption.DB, "db", false, "数据库迁移")
 	flag.BoolVar(&FileOption.Version, "v", false, "版本")
+	flag.StringVar(&FileOption.User, "u", "", "创建用户")
 	flag.Parse()
 }
 
@@ -26,6 +28,14 @@ func Run() {
 		fmt.Println("数据库开始迁移")
 		FlagDB()
 		os.Exit(0)
+	}
+	if FileOption.User == "admin" || FileOption.User == "user" {
+		FlagUser(FileOption.User)
+		fmt.Println("<UNK>")
+
+	} else {
+		//那就是写错了
+		flag.Usage()
 	}
 
 }
---
+++ flags/flag_user.go
@@ -0,0 +1,5 @@
+package flags
+
+func FlagUser(permission string) {
+
+}
